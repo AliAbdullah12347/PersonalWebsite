1c639d014968653d9b9eaf6c05e88c129dd95443
Ali Abdullah <aliabdullah12347@gmail.com>
2025-08-19 19:43:04 -0400

    minor changes

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
 
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span><Link
+          href={"/"}
+          className="text-2xl md:text-5xl text-white font-semibold"
+        >
+          <Image 
+                  src="/images/logo.png" 
+                  alt="Logo" 
+                  width={60} 
+                  height={60} 
+          />
+        </Link>
+        </span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
